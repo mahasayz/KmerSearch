d165cfb2f20b73526d5719f2794ec854300b3dda
Alam, Mahbub (Agoda) <mahbub.alam@agoda.com>
2015-12-06 01:05:13 +0700

    Taking unique doesn't work but still committing

--- src/pattern/core/Main.java
+++ src/pattern/core/Main.java
@@ -13,6 +13,7 @@ public class Main {
     private List<ArrayList<String>> kMers = new ArrayList<ArrayList<String>>();
     private char[] nucleotides = {'A', 'C', 'G', 'T'};
     public static List<HashSet<String>> PS = new ArrayList<HashSet<String>>();
+    public static List<HashSet<String>> finalPS = new ArrayList<HashSet<String>>();
 
     public void generateKmers() {
         ArrayList<String> temp = new ArrayList<String>();
@@ -41,6 +42,11 @@ public class Main {
             for (int j=0; j<main.kMers.size(); j++) {
                 if (j > genome.length())
                     break;
+                // We have 4 nucleotides, just checking to see if it's necessary to generate
+                // k-mers of small value of k
+                if (Math.pow(4,(j+1)) < genomes.length) {
+                    continue;
+                }
 //                System.out.println("Genome : " + genome.toString() + ", kMers size " + main.kMers.size());
                 for (String pattern : main.kMers.get(j)) {
 //                    System.out.println("Looking for : " + pattern);
@@ -49,6 +55,7 @@ public class Main {
                 }
             }
             PS.add(new HashSet<String>(temp));
+            finalPS.add(new HashSet<>(temp));
             temp.clear();
             genome.setLength(0);
         }
@@ -64,15 +71,60 @@ public class Main {
             }
 
             // this is the set DIFFERENCE operation to remove all elements from previous UNION
-            PS.get(i).removeAll(sum);
+            finalPS.get(i).removeAll(sum);
             sum.clear();
         }
 
+        Map<Integer, HashSet<String>> biomarkerCandidates = new HashMap<Integer, HashSet<String>>();
         for (int i=0; i<S.size(); i++) {
             System.out.print("Genome, S(" + i + ") : " + S.get(i).getName() + " - unique pattern(s) are { ");
-            for (String unique : PS.get(i))
+            for (String unique : finalPS.get(i)) {
                 System.out.print(unique + " ");
+                if (biomarkerCandidates.get(unique.length()) == null)
+                    temp.clear();
+                else
+                    temp = biomarkerCandidates.get(unique.length());
+                temp.add(unique);
+                biomarkerCandidates.put(unique.length(), new HashSet<String>(temp));
+            }
             System.out.println("}");
+        }
+
+        temp.clear();
+        ArrayList<String> biomarkers = new ArrayList<String>();
+        boolean complete = false;
+        int candidatesRequired = (int) Math.ceil(Math.log(genomes.length) / Math.log(2));
+        for (Integer val : biomarkerCandidates.keySet()) {
+            temp = biomarkerCandidates.get(val);
+            if (temp.size() < candidatesRequired)
+                continue;
+            for (String str : temp)
+                if (biomarkers.size() < candidatesRequired)
+                    biomarkers.add(str);
+                else {
+                    complete = true;
+                    break;
+                }
+            if (complete)
+                break;
+        }
+
+        System.out.println("number for biomarkers, m = " + biomarkers.size());
+        System.out.println("Unique binary values for genomes are : ");
+        StringBuilder sb = new StringBuilder();
+        for (String gen : genomes) {
+            System.out.print("Genome : " + gen + " ");
+            for (String pattern : biomarkers)
+                if (gen.indexOf(pattern.toLowerCase()) > -1) {
+                    System.out.print("(" + pattern + ") 1 ");
+                    sb.append("1");
+                }
+                else {
+                    System.out.print("(" + pattern + ") 0 ");
+                    sb.append("0");
+                }
+            System.out.print("Decimal Value = " + Integer.parseInt(sb.toString(), 2) + "\n");
+            sb.setLength(0);
         }
 
     }
